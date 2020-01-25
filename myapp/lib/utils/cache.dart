b4f208cc1eca0f90bb9c49837ce4b75fc426eb94
tcollard <tcollard@student.42.fr>
2020-01-25 13:05:09 +0100

    feat: 🎸 (pages + utils) Init route app & pages

---
+++ myapp/lib/utils/cache.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+// import 'dart:convert';
+// import 'package:path_provider/path_provider.dart';
+
+File jsonFile;
+Directory dir;
+String fileName = "cache.json";
+bool fileExists = false;
+Map<String, dynamic> fileContent;
+
+class Cache {
+  // static Future getColor() async {
+
+  // }
+}
---
+++ myapp/lib/utils/routes.dart
@@ -0,0 +1,11 @@
+import 'package:myapp/pages/home.dart';
+import 'package:myapp/pages/recorder.dart';
+import 'package:myapp/pages/settings.dart';
+
+class AllRoutes {
+  static var route = {
+    '/': (context) => HomePage(),
+    '/recorder': (context) => RecorderPage(),
+    '/settinges': (context) => SettingsPage(),
+  };
+}
