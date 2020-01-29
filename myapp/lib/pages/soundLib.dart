0b478d244ccc6ea1808fc398126388d7b62c7fa6
tcollard <tcollard@student.42.fr>
2020-01-29 01:09:09 +0100

    test: 💍 (soundList) Display soundList try order by drag & drop

--- myapp/lib/pages/soundLib.dart
+++ myapp/lib/pages/soundLib.dart
@@ -11,34 +11,86 @@ class SoundLib extends StatefulWidget {
 }
 
 class _SoundLibState extends State<SoundLib> {
-  List<Object> listSound = [];
+  List<Widget> listSound = [];
 
   @override
   void initState() {
-    // listSound = _initSoundList();
-    _initSoundList();
     super.initState();
+    //   initSoundList().then((data) => this.listSound = data);
+    //   print('HELLO $listSound');
+    initSoundList();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(titleAppBar: 'Library'),
       drawer: CustomDrawer(),
-      body: Container(
-        child: Column(
-        //   crossAxisAlignment: CrossAxisAlignment.center,
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: listSound,
-        ),
+      body: ListView(
+        children: this.listSound,
       ),
+      // body: ReorderableListView(
+      //   children: this.listSound,
+      //   onReorder: (oldIndex, newIndex) {
+      //     setState(() {
+      //       updateIndex(oldIndex, newIndex);
+      //     });
+      //   },
+      // ),
     );
   }
 
-   _initSoundList() async {
+  initSoundList() async {
     var recordsJson = await Cache().getRecord();
 
-    List<Widget> truc = [];
+    List<Widget> listTmp = [];
     print('toto => ${recordsJson[0]["name"]}');
-      
-   }
+    for (var i = 0; i < recordsJson.length; i++) {
+      Card card = Card(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.all(12),
+          child: Row(
+            children: <Widget>[
+              Text(recordsJson[i]["name"]),
+              ButtonBar(
+                children: <Widget>[
+                  IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        print('Delete ${recordsJson[i]["name"]}');
+                      }),
+                  IconButton(
+                      icon: Icon(Icons.mode_edit),
+                      onPressed: () {
+                        print('Edit ${recordsJson[i]["name"]}');
+                      }),
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+      listTmp.add(card);
+    }
+    // for (var record in recordsJson) {
+    //   ListTile card = ListTile(
+    //     key: record["name"],
+    //     title: Text(record["name"]),);
+    //   listTmp.add(card);
+    // }
+
+
+    setState(() {
+      this.listSound = listTmp;
+    });
+    // }
+  }
+
+  updateIndex(oldIndex, newIndex) {
+    print('change index: ${listSound[oldIndex]}');
+    ListTile tmp = listSound[oldIndex];
+    listSound[oldIndex] = listSound[newIndex];
+    listSound[newIndex] = tmp;
+  }
 }
